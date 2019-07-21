3b6a34f284f35db368872f9fce23f64015fcec14
rperez <rpg0702@gmail.com>
2019-07-21 18:40:34 +0200

    Issue #58

--- websim/myAlgorithm.js
+++
@@ -1,8 +0,0 @@
-
-// Press Ctrl-, to enter code editor configuration.
-// Write your algorithm inside next function and press RUN.
-// Always use this code above.
-$(document).ready(function() {
-    var myRobot = new RobotI('a-pibot');
-
-});
