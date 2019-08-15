10b3fe0b37301d6d54af6d18bc8401947376266d
rperez <rpg0702@gmail.com>
2019-08-15 20:38:59 +0200

    Issue #73: Now, editor works well

---
+++ simcore/websim2d-world-controlled.js
@@ -0,0 +1,49 @@
+function startStopCode(play, reservedVariables, mainInterval, codeContent){
+  var image = document.getElementById("runbtn").firstChild;
+
+  if(!play){
+    image.src = "../assets/resources/stop-icon.png";
+    codeContent = cleanRedefinition(codeContent, reservedVariables);
+    play = true;
+    console.log(codeContent);
+    eval(codeContent);
+    console.log("Executing code.");
+  }else{
+    image.src = "../assets/resources/play-icon.png";
+    //clearInterval(mainInterval);
+    if (mainInterval && typeof mainInterval === "function"){
+      console.log("hola");
+     mainInterval(); //mainInterval now is a function that clears setIntervalSynchronous
+    }else{
+      console.log("adios")
+    }
+    myRobot.clearVelocity();
+
+    play = false;
+    console.log("Execution stopped.")
+  }
+  return {"play": play, "mainInterval": mainInterval}
+  //return "prueba";
+}
+
+function cleanRedefinition(scriptContent, reservedVariables){
+/*
+  This function removes redefinition of variables "myRobot" and "mainInterval"
+*/
+  var contentSplitted = scriptContent.split("\n");
+  var definitionLine = contentSplitted[1].split(" ");
+  console.log(definitionLine);
+  reservedVariables.forEach((variable, position)=>{
+    var index = definitionLine.indexOf(variable);
+    if(index != -1){
+      if(position == 0 || position == 1){
+        definitionLine[index] = "";
+      }else{
+        definitionLine[index] = "dummyVariable;"
+      }
+    }
+  });
+
+  contentSplitted[1] = definitionLine.join(" ");
+  return contentSplitted.join("\n");
+}
