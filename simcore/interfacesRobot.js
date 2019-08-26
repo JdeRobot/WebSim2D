4b4ac93d11f061fff0639370831291c178a75a15
rperez <rpg0702@gmail.com>
2019-08-26 16:50:26 +0200

    Issue #70: Add ray-cast sensor

--- simcore/interfacesRobot.js
+++ simcore/interfacesRobot.js
@@ -1,9 +1,10 @@
 
 var myRobot;
-
+var map = [];
 var fondo = new Image();
+var nray = 25;
+var raymax = 200;
 fondo.src = "../assets/resources/follow_line.png";
-
 var myScene= {
     canvas : document.createElement("canvas"),
     start : function() {
@@ -55,6 +56,8 @@ function RobotI(width, height, color, x, y, type) {
     this.yInfraredR = this.y;
     this.xInfraredF = this.x;
     this.yInfraredF = this.y;
+    this.raycast;
+    var imgData;
     this.update = function() {
         ctx = myScene.context;
         ctx.drawImage(fondo, 0,0,ctx.canvas.width, ctx.canvas.height);
@@ -70,7 +73,10 @@ function RobotI(width, height, color, x, y, type) {
                  -this.height/2,
                  this.width, this.height);
          ctx.restore();
-
+         imgData = ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height).data;
+         //getMap(imgData,ctx.canvas.width, ctx.canvas.height);
+         this.raycast = setRayCast(this.w,this.x+54*Math.cos((this.w * Math.PI/180)),
+                      this.y-54*Math.sin((this.w * Math.PI/180)), ctx);
     }
     this.newPos = function() {
         this.x += this.v/10*Math.cos(this.w * Math.PI/180);
@@ -94,10 +100,10 @@ function RobotI(width, height, color, x, y, type) {
 
     this.getInfrared = function(){
 
-      this.xInfraredL = this.x+45.6*Math.cos(((this.w * Math.PI/180)+(15.26*Math.PI/180)))
-      this.xInfraredR = this.x+45.6*Math.cos(((this.w * Math.PI/180)+(-15.26*Math.PI/180)))
-      this.yInfraredL = this.y- 45.6*Math.sin(((this.w * Math.PI/180)+(15.26*Math.PI/180)))
-      this.yInfraredR = this.y-45.6*Math.sin(((this.w * Math.PI/180)+(-15.26*Math.PI/180)))
+      this.xInfraredL = this.x+47*Math.cos(((this.w * Math.PI/180)+(15.26*Math.PI/180)))
+      this.xInfraredR = this.x+47*Math.cos(((this.w * Math.PI/180)+(-15.26*Math.PI/180)))
+      this.yInfraredL = this.y- 47*Math.sin(((this.w * Math.PI/180)+(15.26*Math.PI/180)))
+      this.yInfraredR = this.y-47*Math.sin(((this.w * Math.PI/180)+(-15.26*Math.PI/180)))
       this.xInfraredF = this.x+52*Math.cos((this.w * Math.PI/180));
       this.yInfraredF = this.y-52*Math.sin((this.w * Math.PI/180));
       ctx = myScene.context;
@@ -111,6 +117,10 @@ function RobotI(width, height, color, x, y, type) {
       return infrared
     }
 
+    this.getRayCast = function(){
+      return this.raycast;
+    }
+
     this.getW = function(){
       return this.w
     }
@@ -119,10 +129,72 @@ function RobotI(width, height, color, x, y, type) {
       this.v = 0
       this.w = 0
     }
+
 }
 
 function updateScene(){
   myScene.clear();
   myRobot.newPos();
   myRobot.update();
+}
+
+function getMap(pixels, width, height) {
+  var i = 0;
+  for (var y = 0; y < height; y += 1){
+    map[y] = []
+    for (var x = 0; x < width; x += 1){
+      if (pixels[i] == 255 && pixels[i+1] == 255 && pixels[i+2] == 255){
+        map[y][x]=0;
+      } else if (pixels[i]== 0 && pixels[i+1] == 0 && pixels[i+2]==255) {
+        map[y][x]=0;
+      } else {
+        map[y][x]=1;
+      }
+      i += 1
+    }
+  }
+}
+
+
+function setRayCast(angle,x,y, ctx){
+  var raycast = [];
+  var aray = 120/nray;
+  for (var ray = 0; ray < nray; ray++){
+    raycast[ray] = [];
+    var rayangle = (60-ray*aray+angle)*(Math.PI/180)
+    var dx = 0
+    var dy = 0
+    var collision = false;
+    var r = 1;
+    while (!collision && r <=raymax){
+      bx = x + (r-1)*Math.cos(rayangle);
+      by = y - (r-1) * Math.sin(rayangle);
+      dx = x + r*Math.cos(rayangle);
+      dy = y - r * Math.sin(rayangle);
+      var pixel = ctx.getImageData(dx,dy,1,1).data;
+      var bpixel = ctx.getImageData(bx,by,1,1).data;
+      if (pixel[2] > pixel[1] && pixel[2] > pixel[0]){
+        r+=2;
+      } else if (pixel[0] == 255 && pixel[1] == 255 && pixel[2] == 255) {
+        r+=2;
+      } else if (pixel[0] == 0 && pixel[1] == 0 && pixel[2] == 255) {
+        r+=2;
+      } else {
+        collision = true;
+      }
+    }
+    raycast[ray].x = dx;
+    raycast[ray].y = dy;
+  }
+  for (var ray = 0; ray<nray; ray++){
+    dx = raycast[ray].x;
+    dy = raycast[ray].y;
+    ctx.lineWidth = 1;
+		ctx.strokeStyle = "#E6C509";
+		ctx.beginPath();
+		ctx.moveTo(x, y);
+		ctx.lineTo(dx, dy);
+		ctx.stroke();
+  }
+  return raycast
 }
